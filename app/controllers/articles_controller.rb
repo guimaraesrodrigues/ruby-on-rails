08e72014379d0e20006e18cba3c491d0af68bcf5
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2021-07-01 00:17:57 -0300

    add delete feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def delete
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -33,5 +37,11 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
